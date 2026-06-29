f48db33374045d1850da52fc41d06024ae3c4871
taisaokillmik <153414115+taisaokillmik@users.noreply.github.com>
2026-06-29 18:24:45 +0700

    Fix basePath for GitHub Pages subdirectory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/NKHamster",
+  assetPrefix: "/NKHamster",
   compress: true,
   swcMinify: true,
   images: {
